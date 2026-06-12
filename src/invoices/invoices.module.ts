5bd694c8889ce938695ea4eb9e15f29edc4ad4bd
Ali Rakhshanipour <alirakhshanipour@Alis-MacBook-Pro.local>
2026-06-12 13:52:34 +0330

    project done

--- src/invoices/invoices.module.ts
+++ src/invoices/invoices.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { InvoicesService } from './invoices.service';
 import { InvoicesController } from './invoices.controller';
+import { PlansModule } from '../plans/plans.module';
 
 @Module({
+  imports: [PlansModule],
   controllers: [InvoicesController],
   providers: [InvoicesService],
   exports: [InvoicesService],
--- src/tickets/tickets.module.ts
+++ src/tickets/tickets.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TicketsService } from './tickets.service';
 import { TicketsController } from './tickets.controller';
+import { PlansModule } from '../plans/plans.module';
 
 @Module({
+  imports: [PlansModule], // اضافه کردن PlansModule
   controllers: [TicketsController],
   providers: [TicketsService],
   exports: [TicketsService],
